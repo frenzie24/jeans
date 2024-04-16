5db256585a769738116790b116cf57c29278d1bf
frenzie24 <kevincharlesgross@gmail.com>
2024-04-16 13:39:20 -0400

    added example hitObj added const to hold api key implemented key literal to imgQueryURL

--- assets/tests/pixabay-tests/script.js
+++ assets/tests/pixabay-tests/script.js
@@ -3,15 +3,46 @@
         //do work
     })
 */
-
+const APIKey = "43404962-ba2a24215101c788c299fa20a";
+//keyword will be passed as an array or obj 
 function runTest(keyword) {
-    let imgQueryURL = `https://pixabay.com/api/?key=43404962-ba2a24215101c788c299fa20a&q=yellow+flowers&image_type=photo`;
+
+    let keywords = ""; 
+    keyword.forEach(key => {
+        keywords += "+"+key;
+    })
+    //let imgQueryURL = `https://pixabay.com/api/?key=`;
+    let imgQueryURL = `https://pixabay.com/api/?key=${APIKey}&q=yellow+flowers&image_type=photo`;
     fetch(imgQueryURL).then(response => response.json()).then(result => {
         console.log(result);
         let hits = result.hits;
         hits.forEach(hit => {
             console.log(hit);
         });
+        let hit = hits[0];
+        /* hit OBJ: */
+        let hitObj = {
+            imageSize: hit.imageSize, // what units are these???
+            imageHeight: hit.imageHeight,
+            imageWidth: hit.imageWidth,
+            tags: hit.tags,
+            type: hit.type,
+            user: hit.user,
+            userImageURL: hit.user,
+            user_id: hit.user_id,
+            imageURL: hit.imageURL,
+            largeImageURL: hit.largeImageURL,
+            id: hit.id,
+            id_hash: hit.id_hash,
+            webformatHeight: hit.webformatHeight,
+            webformatWidth: hit.webformatWidth,
+            pageURL: hit.pageURL,
+            previewURL: hit.previewURL,
+            previewHeight: hit.previewHeight,
+            previewWidth: hit.previewWidth,
+            fullHDURL: hit.fullHDURL,
+        }
+        console.log(hitObj)
         debugger;
         //do work
     })
