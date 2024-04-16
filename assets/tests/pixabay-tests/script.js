2df94e23510b5ff42268808cfc3626a7ae6bf009
frenzie24 <kevincharlesgross@gmail.com>
2024-04-16 13:46:57 -0400

    created and implemented string literal for query string tested single query usage with string literal passed created logic to parse mutltiple keywords

--- assets/tests/pixabay-tests/script.js
+++ assets/tests/pixabay-tests/script.js
@@ -7,12 +7,20 @@ const APIKey = "43404962-ba2a24215101c788c299fa20a";
 //keyword will be passed as an array or obj 
 function runTest(keyword) {
 
-    let keywords = ""; 
-    keyword.forEach(key => {
-        keywords += "+"+key;
-    })
+
+    let keywords = "";
+    // keyword = ['yellow', 'red']; => needs to be: 'yellow+red'
+    if (typeof (keyword) == Array) {
+        keyword.forEach(key => {
+            keywords += "+" + key; // currently outputs +yellow+red
+        })
+    }
+    console.log(keywords);
+    keywords = keywords.splice(0, 1, "");
+    console.log(keywords);
+
     //let imgQueryURL = `https://pixabay.com/api/?key=`;
-    let imgQueryURL = `https://pixabay.com/api/?key=${APIKey}&q=yellow+flowers&image_type=photo`;
+    let imgQueryURL = `https://pixabay.com/api/?key=${APIKey}&q=${keyword}&image_type=photo` //yellow+flowers&image_type=photo`;
     fetch(imgQueryURL).then(response => response.json()).then(result => {
         console.log(result);
         let hits = result.hits;
