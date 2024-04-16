86a5ed9272616cf5a265cd709312e03ff2e2800b
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-04-16 14:23:55 -0400

    created basic rules for naming conventions

---
+++ naming.js
@@ -0,0 +1,9 @@
+// all variables will be listed in camelCase
+
+// variables for constant elements will be named in the format of [subject][detail]El, using an example of fontInputEl.
+
+// functions for creating information onto page will be in the format of create[subject]() such as createCard()
+
+// functions for saving information will be in save[subject]Data. functions for reading will be read[subject]Data. 
+
+// functions for form information sent in will be in the format of [subject]Submit.
