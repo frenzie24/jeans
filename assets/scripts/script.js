5e9ee57fea73c6cbe04ad7d9ea18cee53591f105
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-04-17 16:48:46 -0400

    movest test function for colors

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,36 @@
 const pk = "43404962-ba2a24215101c788c299fa20a";
+
+let ps = 'https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=json&mode=analogic&count=6'
+// "https://www.thecolorapi.com/id?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=json"
+/*
+"https://www.thecolorapi.com/id
+?hex=0047AB
+&rgb=0,71,171
+&hsl=215,100%,34%
+&cmyk=100,58,0,33
+&format=html"
+*/
+
+function runTest(colorData) {
+    let qs = `https://www.thecolorapi.com/id?hex=${colorData}&format=json`
+    fetch(qs).then(result => result.json()).then(result => {
+        debugger;
+    });
+
+}
+/*
+'https://www.thecolorapi.com/scheme
+?hex=0047AB
+&rgb=0,71,171
+&hsl=215,100%,34%
+&cmyk=100,58,0,33
+&format=json
+&mode=analogic
+&count=6'
+*/
+
+function runSchemeTest(colorData) {
+    fetch(ps).then(result => result.json()).then(result => {
+        debugger;
+    });
+}
