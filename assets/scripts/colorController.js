eaa646183cfcdb46ae1be3c463faffff5ef56e13
frenzie24 <kevincharlesgross@gmail.com>
2024-04-21 13:41:31 -0400

    removed stringToColor, was unused

--- assets/scripts/colorController.js
+++ assets/scripts/colorController.js
@@ -166,18 +166,7 @@ function testGenerics() {
 }
 
 // takes string entered in search bar and makes it into a format that can be compared to saved list of names
-const stringToColour = (str) => {
-    let hash = 0;
-    str.split('').forEach(char => {
-        hash = char.charCodeAt(0) + ((hash << 5) - hash)
-    })
-    let colour = '#'
-    for (let i = 0; i < 3; i++) {
-        const value = (hash >> (i * 8)) & 0xff
-        colour += value.toString(16).padStart(2, '0')
-    }
-    getColorByHex(colour);
-}
+
 
 function renderSchemes(colorData) {
     localStorage.setItem('scheme', colorData);
